a435e0f0db4780f0b3c63214c222a156070b1fb7
Stanlyzoolo <stanlyzoolo@gmail.com>
2021-07-13 23:14:02 +0300

    http rest service

---
+++ go.mod
@@ -0,0 +1,5 @@
+module restbasiccalc
+
+go 1.16
+
+require github.com/stanlyzoolo/basiccalc v0.0.0-20210706163310-898b1cbc38b3
--- restbasiccalc.go
+++ restbasiccalc.go
@@ -1,28 +1,41 @@
-package restbasiccalc
+package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
+	. "github.com/stanlyzoolo/basiccalc"
+)
 
-
-type exprRequest struct {
-	expression string
+type ReturnData struct {
+	Result int    `json:"result"`
+	Error  error  `json:"error"`
+	Expr   string `json:"expr"`
 }
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// логика обработчика
 
-	
+	var expr = r.URL.Query().Get("expr")
+	// var expr = r.URL.Query()["expr"][1]
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// fmt.Fprint(w, "Hello: ", expr)
+
+	result, err := Eval(expr)
+	data, _ := json.Marshal(ReturnData{result, err, expr})
+	w.Write(data)
+	// fmt.Fprintf(w, "Result: %v. Error: %s", result, err)
 }
 
+func main() {
 
+	http.HandleFunc("/", handler)
 
-func serverCalc() {
-
-	http.HandleFunc("/basiccalc", handler)
-
+	http.HandleFunc("/test", handler)
 
 	http.ListenAndServe(":8080", nil)
-	// логика
-	
 }
+
+// Gracefully shutdown - читать
+// os.signals - читать
