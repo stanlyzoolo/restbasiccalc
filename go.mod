b905c0b9658216c6aafaae44448acf1ad1dff767
Stanlyzoolo <stanlyzoolo@gmail.com>
2021-07-29 23:50:00 +0300

    fixed all golangci-lint issues

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module restbasiccalc
 
 go 1.16
 
-require github.com/stanlyzoolo/basiccalc v0.0.0-20210706163310-898b1cbc38b3
+require (
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/go-playground/validator/v10 v10.8.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mgechev/revive v1.0.9 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/namsral/flag v1.7.4-pre
+	github.com/stanlyzoolo/basiccalc v0.0.0-20210706163310-898b1cbc38b3
+	github.com/ugorji/go v1.2.6 // indirect
+	go.uber.org/zap v1.18.1
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- restbasiccalc.go
+++ restbasiccalc.go
@@ -3,57 +3,92 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
-	. "github.com/stanlyzoolo/basiccalc"
+	"github.com/namsral/flag"
+	"github.com/stanlyzoolo/basiccalc"
+	"go.uber.org/zap"
 )
 
-type ReturnData struct {
+type returnData struct {
 	Result int    `json:"result"`
 	Error  error  `json:"error"`
 	Expr   string `json:"expr"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
 	var expr = r.URL.Query().Get("expr")
 
 	w.Header().Set("Content-Type", "application/json")
 
-	result, err := Eval(expr)
-	data, _ := json.Marshal(ReturnData{result, err, expr})
-	w.Write(data)
+	logger, _ := zap.NewDevelopment()
+	result, err := basiccalc.Eval(expr)
+
+	if err != nil {
+		w.WriteHeader(400) //nolint
+		logger.Error("failed evaluating an expression",
+			zap.String("400", "Bad Request"),
+			zap.String("package", "restbasiccalc"),
+			zap.Error(err))
+	}
+
+	data, err := json.Marshal(returnData{result, err, expr})
+	if err != nil {
+		w.WriteHeader(500) //nolint
+		logger.Error("failed evaluating an expression",
+			zap.String("500", "Internal Server Error"),
+			zap.String("package", "restbasiccalc"),
+			zap.Error(err))
+	}
+
+	w.Write(data) //nolint
 }
 
 func main() {
+	var port = 8080
 
-	srv := &http.Server{
-		Addr:    ":8080",
+	flag.IntVar(&port, "port", port, "Port number")
+	flag.Parse()
+	// nolint
+	var srv = &http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
 		Handler: http.HandlerFunc(handler),
 	}
 
 	go func() {
-
-		gracefulShDw := make(chan os.Signal, 1)
-		signal.Notify(gracefulShDw, os.Interrupt)
-		<-gracefulShDw
-
-		if err := srv.Shutdown(context.Background()); err != nil {
-			fmt.Printf("HTTP server Shutdown: %v", err)
+		if err := srv.ListenAndServe(); errors.Is(err, http.ErrServerClosed) {
+			logger, _ := zap.NewDevelopment()
+			logger.Fatal("failed server start",
+				zap.String("package", "restbasiccalc"),
+				zap.String("func", "ListenAndServe"),
+				zap.Error(err),
+			)
 		}
-
-		close(gracefulShDw)
 	}()
 
-	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-		fmt.Printf("HTTP server ListenAndServe: %v", err)
+	logger, _ := zap.NewDevelopment()
+	logger.Info("server started")
 
+	gracefulShDw := make(chan os.Signal, 1)
+	signal.Notify(gracefulShDw, os.Interrupt)
+
+	<-gracefulShDw
+	logger.Info("server stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) //nolint
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger, _ := zap.NewDevelopment()
+		logger.Fatal("failed shutdown server",
+			zap.String("package", "restbasiccalc"),
+			zap.String("func", "Shutdown"),
+			zap.Error(err),
+		)
 	}
-
 }
-
-// Gracefully shutdown - читать
-// os.signals - читать
